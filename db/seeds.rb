4e221ed6c13c5851ce08b3b766e7a21f227a5238
Pierre Deville <pierre@pierredeville.com>
2022-07-05 15:41:34 +0200

    Exercice 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Reset les données de seed
+Article.destroy_all
+
+5.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs.join("\n")
+  )
+end
+
+5.times do
+  Article.create!(
+    title: Faker::Game.title,
+    content: Faker::Lorem.paragraphs.join("\n")
+  )
+end
+
+5.times do
+  Article.create!(
+    title: Faker::TvShows::Buffy.quote,
+    content: Faker::Lorem.paragraphs.join("\n")
+  )
+end
+
+
